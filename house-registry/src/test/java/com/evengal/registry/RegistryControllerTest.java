962830e97ce32f78b440db0fb58f99e1ddec25e5
EvenGal <even.galaasen@hotmail.com>
2024-09-06 13:30:12 +0200

    add exception handling

---
+++ house-registry/src/test/java/com/evengal/registry/RegistryControllerTest.java
@@ -0,0 +1,10 @@
+package com.evengal.registry;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class RegistryControllerTest {
+
+}
