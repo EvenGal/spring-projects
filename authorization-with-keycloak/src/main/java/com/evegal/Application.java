b435eed64d6a541c19930e1abb20f7a9930584dc
EvenGal <even.galaasen@hotmail.com>
2024-09-27 09:36:12 +0200

    fix maven wrapper

--- authorization-with-keycloak/src/main/java/com/evegal/Application.java
+++ authorization-with-keycloak/src/main/java/com/evegal/Application.java
@@ -1,13 +1,13 @@
-package com.example.authorization_with_keycloak;
+package com.evegal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
